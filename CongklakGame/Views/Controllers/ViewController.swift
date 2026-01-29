efd7075ffe30fc3f6da29cc2e8577ca8f635dceb
Irwan Cannady <irwancannadys@gmail.com>
2026-01-29 11:39:05 +0700

    chore: reinitialize project with UIKit/Storyboard

---
+++ CongklakGame/Views/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CongklakGame
+//
+//  Created by irwan on 29/01/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CongklakGameTests/CongklakGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  CongklakGameTests.swift
+//  CongklakGameTests
+//
+//  Created by irwan on 29/01/26.
+//
+
+import Testing
+@testable import CongklakGame
+
+struct CongklakGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
